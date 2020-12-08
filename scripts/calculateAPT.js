87e240b064deffe1b0e9d4e9f8ddf3d5cc8580a7
NotARanger97 <patatombolo@gmail.com>
2020-12-08 14:46:15 +0100

    changhed to bootstrap and modified Calculations

--- scripts/calculateAPT.js
+++ scripts/calculateAPT.js
@@ -10,7 +10,7 @@ function calculateAPT(){
 	var dm = document.getElementById("dm").value;
 	var SE = 1;
 	if (atk == 0 || ls == 0 || links == 0 || passive == 0
-		|| ki == 0 || crit == 0 || add == 0 || dm == 0){
+		|| ki == 0 || dm == 0){
 		window.alert("parameters empty, please enter all the parameters");
 		exit();
 	}
@@ -18,9 +18,17 @@ function calculateAPT(){
 		SE = 1.5;
 	var preSA = atk * ls * links * passive * ki;
 	var SA = preSA * dm;
-	var fixedcrit = (1.9 * crit) + (SE * (1-crit));
-	var SACrit = (SA * fixedcrit);
-	var preSACrit = (preSA * fixedcrit);
-	result = SACrit + (SACrit * add) + (preSACrit * add);
-	window.alert("your APT is: " + result.toFixed(2));
+	var SACrit = SA * 1.9 * crit;
+	var SAnoCrit = SA * SE * (1-crit);
+	var SATOT = SACrit + SAnoCrit;
+	var preSACrit = preSA * 1.9 * crit;
+	var preSAnoCrit = preSA * SE * (1-crit);
+	var preSATOT = preSACrit + preSAnoCrit;
+	result = SATOT + (SATOT * add) + (preSATOT * add);
+	var value = result.toLocaleString(
+  	undefined, // leave undefined to use the browser's locale,
+             // or use a string like 'en-US' to override it.
+  	{ minimumFractionDigits: 2 }
+	);
+	window.alert("your APT is: " + value);
 }
